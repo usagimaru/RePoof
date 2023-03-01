36298a4657263635517c23c8b116b7cf40ed339c
usagimaru <usagimaruma@gmail.com>
2023-03-02 01:44:00 +0900

    Initial Commit

---
+++ RePoof/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  RePoof
+//
+//  Created by usagimaru on 2023/03/02.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
